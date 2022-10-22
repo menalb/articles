2b6e74aaddca351d93612a0d2a6d6030626b66fc
Alberto Meneghini <menalb84@gmail.com>
2022-10-22 20:17:00 +0200

    refactoring flag service

--- blazor/blazor-awsappconfig/src/blazor-appconfig/FlagsStorageService.cs
+++ blazor/blazor-awsappconfig/src/blazor-appconfig/FlagsStorageService.cs
@@ -1,35 +1,11 @@
-﻿using System.Net.Http.Json;
-
-namespace blazor_appconfig.Services;
-
-public class FlagsService
-{
-    private readonly HttpClient _http;
-
-    public FlagsService(HttpClient http)
-    {
-        _http = http ?? throw new ArgumentNullException(nameof(http));
-    }
-
-    public async Task<IEnumerable<string>> GetFlags()
-    {
-        var flag = await _http.GetFromJsonAsync<FlagResponse>("mansions");
-        if (flag != null && flag.Enabled)
-        {
-            return new List<string> { nameof(Components.StepsListWizardComponent) };
-        }
-        return new List<string>();
-    }
-
-    public record FlagResponse(bool Enabled);
-}
+﻿namespace blazor_appconfig.Services;
 
 public class FlagsStorageService
 {
     private IEnumerable<string> Flags = new List<string>();
-    
+
     public void Init(IEnumerable<string> flags)
-    {        
+    {
         if (flags != null)
         {
             Flags = flags;
@@ -37,7 +13,5 @@ public class FlagsStorageService
     }
 
     public bool IsRecipeStepsWizardEnabled()
-    {
-        return Flags.Any(f=>f == nameof(Components.StepsListWizardComponent));
-    }
+     => Flags.Any(f => f == nameof(Components.StepsListWizardComponent));
 }
